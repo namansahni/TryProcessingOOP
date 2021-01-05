fe6d95c310f3f7b9a8d65b9e7aed4f4392aa7033
namansahni <naman.sahni10@gmail.com>
2021-01-05 19:23:55 +0530

    Refactored TryProcessingOOP & Circle classes

--- TryProcessing/src/main/java/TryProcessingOOP.java
+++ TryProcessing/src/main/java/TryProcessingOOP.java
@@ -1,18 +1,17 @@
 import processing.core.PApplet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TryProcessingOOP extends PApplet {
 
     public static final int WIDTH = 640;
     public static final int HEIGHT = 480;
     public static final int DIAMETER = 10;
-    private static final int NO_OF_CIRCLES = 4;
-    public static float x_pos = 0;
+    public static float xPos = 0;
+    public static final int yAxisDivider = 5;
 
-    EllipseMaker ellipseMaker1;
-    EllipseMaker ellipseMaker2;
-    EllipseMaker ellipseMaker3;
-    EllipseMaker ellipseMaker4;
-
+    List<Circle> circle = new ArrayList<>();
 
     public static void main(String args[]) {
         PApplet.main("TryProcessingOOP", args);
@@ -28,23 +27,21 @@ public class TryProcessingOOP extends PApplet {
     public void setup() {
         super.setup();
 
-        float y_pos = (HEIGHT) / 5;
-        ellipseMaker1 = new EllipseMaker(x_pos, 1 * y_pos, DIAMETER, DIAMETER, 1);
-        ellipseMaker2 = new EllipseMaker(x_pos, 2 * y_pos, DIAMETER, DIAMETER, 2);
-        ellipseMaker3 = new EllipseMaker(x_pos, 3 * y_pos, DIAMETER, DIAMETER, 3);
-        ellipseMaker4 = new EllipseMaker(x_pos, 4 * y_pos, DIAMETER, DIAMETER, 4);
+        float yPos = HEIGHT / yAxisDivider;
+        for (int speed = 1; speed < 5; speed++) {
+            circle.add(new Circle(xPos, speed * yPos, DIAMETER, speed, this));
+        }
     }
 
     @Override
     public void draw() {
-        drawSketch();
+        drawCircle();
     }
 
-    private void drawSketch() {
-        ellipse(ellipseMaker1.moveX_coordinate(),ellipseMaker1.getY_coordinate(),ellipseMaker1.getWidth(),ellipseMaker1.getHeight());
-        ellipse(ellipseMaker2.moveX_coordinate(),ellipseMaker2.getY_coordinate(),ellipseMaker2.getWidth(),ellipseMaker2.getHeight());
-        ellipse(ellipseMaker3.moveX_coordinate(),ellipseMaker3.getY_coordinate(),ellipseMaker3.getWidth(),ellipseMaker3.getHeight());
-        ellipse(ellipseMaker4.moveX_coordinate(),ellipseMaker4.getY_coordinate(),ellipseMaker4.getWidth(),ellipseMaker4.getHeight());
+    private void drawCircle() {
+        for (int speed = 1; speed < 5; speed++) {
+            circle.get(speed - 1).make(); // 0 based indexing
+        }
     }
 
 }
